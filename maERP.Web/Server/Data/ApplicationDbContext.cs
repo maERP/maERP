7002607f60c3310bc8ca0caf8d84fc9206aa3500
Martin <martin.andrich@gmail.com>
2022-10-31 19:42:20 +0100

    add maERP.Web WASM Hosted

---
+++ maERP.Web/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using maERP.Web.Server.Models;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace maERP.Web.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
---
+++ maERP.Web/Server/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace maERP.Web.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
