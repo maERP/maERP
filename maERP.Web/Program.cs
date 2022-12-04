799dc0191d9b8de30deec8ba8d6461c16bfcce39
martin <martin.andrich@gmail.com>
2022-12-04 21:37:16 +0100

    add web project

--- maERP.Web/Program.cs
+++ maERP.Web/Program.cs
@@ -1,6 +1,11 @@
-using maERP.Web;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using maERP.Web;
+using maERP.Shared.Contracts;
+using maERP.Shared.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,11 +13,18 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+/*
 builder.Services.AddOidcAuthentication(options =>
 {
     // Configure your authentication provider options here.
     // For more information, see https://aka.ms/blazor-standalone-auth
     builder.Configuration.Bind("Local", options.ProviderOptions);
 });
+*/
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<AuthStateProvider>());
+
+builder.Services.AddScoped(typeof(IDataService<>), typeof(DataService<>));
 
 await builder.Build().RunAsync();
