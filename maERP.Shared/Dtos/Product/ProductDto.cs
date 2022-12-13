ce98ea194ae8ae46f9ecb54e8ba4be2a7703b52d
martin <martin.andrich@gmail.com>
2022-12-13 20:25:58 +0100

    update nuget

--- maERP.Shared/Dtos/Product/ProductDto.cs
+++ maERP.Shared/Dtos/Product/ProductDto.cs
@@ -8,6 +8,5 @@ public class ProductDto : ProductBaseDto
 {
     public string Name { get; set; }
     public decimal Price { get; set; }
-    public string EAN { get; set; }
     public List<ProductSalesChannelDto> ProductSalesChannel { get; set; }
 }
