28a1bf4c31dec639ba77270eff960f964555aba0
martin <martin.andrich@gmail.com>
2023-03-24 07:51:10 +0100

    bugfix

--- maERP.Shared/Models/Customer.cs
+++ maERP.Shared/Models/Customer.cs
@@ -11,17 +11,19 @@ public class Customer
     [Required]
     [StringLength(50)]
     [Display(Name = "Last Name")]
-    public string LastName { get; set; }
+    public string LastName { get; set; } = string.Empty;
 
     [Required]
     [StringLength(50)]
     [Column("FirstName")]
     [Display(Name = "First Name")]
-    public string FirstName { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+
     [DataType(DataType.Date)]
     [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
     [Display(Name = "Enrollment Date")]
     public DateTime EnrollmentDate { get; set; }
+
     [Display(Name = "Full Name")]
     public string FullName
     {
