7a029e10507a2b305fb338c7e3c483f67885a453
Martin <martin.andrich@gmail.com>
2023-06-11 22:24:02 +0200

    work in progress

---
+++ maERP.Shared/Services/AuthService.cs
@@ -0,0 +1,85 @@
+﻿using System.ComponentModel.DataAnnotations;
+using maERP.Shared.Shared;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace maERP.Shared.Services;
+
+public interface IAuthService
+{
+    Task<ServiceResponse<int>> Register(UserRegister request);
+    Task<ServiceResponse<string>> Login(UserLogin request);
+    Task<ServiceResponse<bool>> ChangePassword(UserChangePassword request);
+    Task<bool> IsUserAuthenticated();
+}
+
+public class AuthService : IAuthService
+{
+    private readonly HttpClient _http;
+    private readonly AuthenticationStateProvider _authStateProvider;
+
+    public AuthService(HttpClient http, AuthenticationStateProvider authStateProvider)
+    {
+        _http = http;
+        _authStateProvider = authStateProvider;
+    }
+
+    public async Task<ServiceResponse<bool>> ChangePassword(UserChangePassword request)
+    {
+        var result = await _http.PostAsJsonAsync("api/auth/change-password", request.Password);
+        return await result.Content.ReadFromJsonAsync<ServiceResponse<bool>>();
+    }
+
+    public async Task<bool> IsUserAuthenticated()
+    {
+        return (await _authStateProvider.GetAuthenticationStateAsync()).User.Identity.IsAuthenticated;
+    }
+
+    public async Task<ServiceResponse<string>> Login(UserLogin request)
+    {
+        var result = await _http.PostAsJsonAsync("api/auth/login", request);
+        return await result.Content.ReadFromJsonAsync<ServiceResponse<string>>();
+    }
+
+    public async Task<ServiceResponse<int>> Register(UserRegister request)
+    {
+        var result = await _http.PostAsJsonAsync("api/auth/register", request);
+        return await result.Content.ReadFromJsonAsync<ServiceResponse<int>>();
+    }
+}
+
+public class User
+{
+    public int Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
+    public DateTime DateCreated { get; set; } = DateTime.Now;
+    public string Role { get; set; } = "Customer";
+}
+
+public class UserChangePassword
+{
+    [Required, StringLength(100, MinimumLength = 6)]
+    public string Password { get; set; } = string.Empty;
+    [Compare("Password", ErrorMessage = "The passwords do not match.")]
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
+
+public class UserLogin
+{
+    [Required]
+    public string Email { get; set; } = string.Empty;
+    [Required]
+    public string Password { get; set; } = string.Empty;
+}
+
+public class UserRegister
+{
+    [Required, EmailAddress]
+    public string Email { get; set; } = string.Empty;
+    [Required, StringLength(100, MinimumLength = 6)]
+    public string Password { get; set; } = string.Empty;
+    [Compare("Password", ErrorMessage = "The passwords do not match.")]
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
--- maERP.Web/Program.cs
+++ maERP.Web/Program.cs
@@ -11,12 +11,15 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IAuthService, AuthService>();
+// builder.Services.AddScoped<AuthStateProvider>();
+// builder.Services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<AuthStateProvider>());
+
+//builder.Services.AddScoped<IClientTokenService, ClientTokenService>();
+// builder.Services.AddScoped(typeof(IDataService<>), typeof(DataService<>));
+
 builder.Services.AddAuthorizationCore();
-
-builder.Services.AddScoped<AuthStateProvider>();
-builder.Services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<AuthStateProvider>());
-
-builder.Services.AddScoped<IClientTokenService, ClientTokenService>();
-builder.Services.AddScoped(typeof(IDataService<>), typeof(DataService<>));
+builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider2>();
 
 await builder.Build().RunAsync();
