66c0d3de265679b5d8bc2c5b5bdad9361e124064
martin <martin.andrich@gmail.com>
2023-07-17 22:13:37 +0200

    remove bootstrap, add admindashboard-theme

---
+++ maERP.Shared/Shared/MainLayout.razor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using maERP.Shared.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using MudBlazor;
+
+namespace maERP.Shared.Shared;
+
+public partial class MainLayout : LayoutComponentBase
+{
+    public MudThemeProvider _mudThemeProvider = null!;
+    public bool _defaultDarkMode;
+
+    public string PrimaryColor { get; set; } = "#2d4275";
+    public string SecondaryColor { get; set; } = "#ff4081ff";
+    public double BorderRadius { get; set; } = 4;
+    public double DefaultFontSize { get; set; } = 0.8125;
+    public MudTheme CurrentTheme { get; private set; } = new();
+    public event EventHandler? MajorUpdateOccured;
+
+    private void OnMajorUpdateOccured() => MajorUpdateOccured?.Invoke(this, EventArgs.Empty);
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+        if (firstRender)
+        {
+            await _mudThemeProvider.WatchSystemPreference(OnSystemPreferenceChanged);
+            StateHasChanged();
+        }
+    }
+
+    private async Task OnSystemPreferenceChanged(bool newValue)
+    {
+        _defaultDarkMode = newValue;
+        await Task.CompletedTask;
+    }
+
+    protected override void OnInitialized()
+    {
+        _defaultDarkMode = false; //  await _mudThemeProvider.GetSystemPreference();
+
+        CurrentTheme = Theme.ApplicationTheme();
+        CurrentTheme.Palette.Primary = PrimaryColor;
+        CurrentTheme.PaletteDark.Primary = PrimaryColor;
+        CurrentTheme.LayoutProperties.DefaultBorderRadius = BorderRadius + "px";
+
+        OnMajorUpdateOccured();
+    }
+
+    public async void IsDarkMode()
+    {
+        _defaultDarkMode = await _mudThemeProvider.GetSystemPreference();
+    }
+}
