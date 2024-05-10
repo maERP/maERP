89b6bb0e6efdb73a33b684d056dd877ee056a358
Martin <ma@martin-andrich.de>
2024-05-10 20:51:54 +0200

    fix tests

--- maERP.Server/Program.cs
+++ maERP.Server/Program.cs
@@ -135,17 +135,6 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    
-    var context = services.GetRequiredService<MaErpIdentityDbContext>();
-    if (context.Database.IsRelational() && context.Database.GetPendingMigrations().Any())
-    {
-        context.Database.Migrate();
-    }
-}
-
 app.Run();
 
 public partial class Program { }
