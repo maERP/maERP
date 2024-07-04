2ec604a819a036eeae43c511f910f6bf4d966b94
Martin <ma@martin-andrich.de>
2024-07-04 21:59:57 +0200

    bugfix

--- maERP.AI/Services/AiService.cs
+++ maERP.AI/Services/AiService.cs
@@ -2,8 +2,8 @@ namespace maERP.AI.Services;
 
 public abstract class AiService
 {
-    private object _api;
-    private object _chat;
+    private object _api = new();
+    private object _chat = new();
     
     public abstract void StartNewChat();
     public abstract Task<string> AskAsync(string question);
