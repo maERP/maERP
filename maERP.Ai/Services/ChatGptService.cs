dffe550221c5f11359380f9399636f1d15998815
Martin <ma@martin-andrich.de>
2024-09-19 20:14:21 +0200

    Nuget WarningsNotAsErrors

--- maERP.Ai/Services/ChatGptService.cs
+++ maERP.Ai/Services/ChatGptService.cs
@@ -1,26 +1,24 @@
 using maERP.Application.Contracts.Ai;
-using OpenAI_API;
-using OpenAI_API.Chat;
-using OpenAI_API.Models;
+using OpenAI.Chat;
 
 namespace maERP.Ai.Services;
 
 public class ChatGptService : AiService, IChatGptService
 {
-    private OpenAIAPI _api = new();
-    private Conversation _chat;
+    private ChatClient _client;
+    private ChatCompletion _chat;
+
+    public ChatGptService()
+    {
+        _client = new ChatClient("gpt-4o", "OPENAI_API_KEY");
+        _chat = _client.CompleteChat(
+        [
+            new UserChatMessage("Say 'this is a test.'"),
+        ]);
+    }
 
     public override void StartNewChat()
     {
-        _api = new OpenAIAPI("YOUR_API_KEY_GOES_HERE"); 
-        _api.Chat.CreateConversation();
-        _chat.Model = Model.GPT4_Turbo;
-        _chat.RequestParameters.Temperature = 0;
-    }
-    
-    public override async Task<string> AskAsync(string question)
-    {
-        _chat.AppendUserInput(question);
-        return await _chat.GetResponseFromChatbotAsync();
+        
     }
 }
