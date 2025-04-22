4c60818217c1e4135696b6ff66be9b55aecb5cc0
Martin <ma@martin-andrich.de>
2025-04-22 21:48:55 +0200

    fix tests

--- src/maERP.Persistence/DatabaseContext/ApplicationDbContext.cs
+++ src/maERP.Persistence/DatabaseContext/ApplicationDbContext.cs
@@ -22,9 +22,6 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
         modelBuilder.ApplyConfiguration(new WarehouseConfiguration());
         modelBuilder.ApplyConfiguration(new SalesChannelConfiguration());
         modelBuilder.ApplyConfiguration(new TaxClassConfiguration());
-
-        // Seed Settings
-        modelBuilder.SeedSettings();
     }
 
     public DbSet<AiModel> AiModel { get; set; } = null!;
