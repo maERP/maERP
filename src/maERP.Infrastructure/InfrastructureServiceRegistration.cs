2ab93ce4a5bbb546558792b8cf77cf6120e01698
Martin <ma@martin-andrich.de>
2025-05-04 12:08:51 +0200

    cleanup

--- src/maERP.Infrastructure/InfrastructureServiceRegistration.cs
+++ src/maERP.Infrastructure/InfrastructureServiceRegistration.cs
@@ -1,8 +1,8 @@
-﻿using maERP.Application.Contracts.Email;
+﻿using maERP.Application.Contracts.Infrastructure;
 using maERP.Application.Contracts.Logging;
 using maERP.Application.Models.Email;
-using maERP.Infrastructure.EmailService;
 using maERP.Infrastructure.Logging;
+using maERP.Infrastructure.PDF;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -13,8 +13,11 @@ public static class InfrastructureServiceRegistration
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
-        services.AddScoped<IEmailSender, EmailSender>();
+        services.AddScoped<IEmailService, EmailService.EmailService>();
         services.AddScoped(typeof(IAppLogger<>), typeof(LoggerAdapter<>));
+        
+        services.Configure<PdfOptions>(configuration.GetSection("PdfOptions"));
+        services.AddScoped<IPdfService, PdfService>();
 
         return services;
     }
