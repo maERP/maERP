8d4880ad8c8907a9567233f8009053b2412e2e56
Martin <ma@martin-andrich.de>
2025-06-07 11:31:15 +0200

    cleanup

--- src/maERP.Web/README.md
+++
@@ -1,5 +0,0 @@
-
-
-```
-docker run -p 80:80 -e MAERP_SERVER_BASE_URL=https://api.dein-server.de maerp-web
-````
--- src/maERP.Web/start.sh
+++
@@ -1,4 +0,0 @@
-﻿#!/bin/sh
-
-jq '.ServerBaseUrl = "$MAERP_SERVER_BASE_URLI"' "$WWWROOT/appsettings.json" | sponge "$WWWROOT/appsettings.json"
-jq '.RemoteLog = $MAERP_REMOTE_LOG' "$WWWROOT/appsettings.json" | sponge "$WWWROOT/appsettings.json"
